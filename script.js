bd588d8d0d0af1a9ed4fc83e50c563d091fbddf7
harshsingh31 <harsh.singh312001@gmail.com>
2023-09-08 20:37:32 +0530

    calculator complete

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ Array.from(buttons).forEach((button)=>{
             string = ""
             document.querySelector('input').value = string
         }
+        else if(e.target.innerHTML == 'del') {
+            string = string.substring(0, string.length-1)
+            document.querySelector('input').value = string
+        }
         else {
             console.log(e.target)
             string = string + e.target.innerHTML
